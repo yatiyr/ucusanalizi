6c10108382162055757c3a815ee192ad0cd7581d
yatiyr <dereeren@yahoo.com>
2020-10-19 20:07:20 +0300

    animation methods with bezier timings added.

--- src/analiz/Analiz.tsx
+++ src/analiz/Analiz.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-var */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, {Component} from 'react';
-import TurkiyeBayrak from '../../resources/svgs/turkiyeBayrak.svg';
-import {givePointOnBezier, timeFunction, move} from '../utils/animations';
+import Ucak from '../../resources/svgs/ucak.svg';
+import {animate} from '../utils/animations';
 
 type Props = any;
 type State = {
@@ -21,18 +22,43 @@ class Analiz extends Component<Props,State> {
     
     constructor(props : any) {
         super(props);
+
+        this.step = this.step.bind(this);
+        this.animateSvg = this.animateSvg.bind(this);
+        this.stepSvgDraw = this.stepSvgDraw.bind(this);
+    }
+
+    step(element : any, progressFunc : any) {
+        var from = 0;
+        var to   = 300;
+
+        var pro = from + (to-from)*progressFunc;
+        element.style.left = pro + 'px';
     }
 
     componentDidMount() {
         // const point = givePointOnBezier([{x: 0, y: 0},{x:10, y:10}], .55);
-        // console.log(point);
-        console.log(timeFunction([{x: .5, y: 1}], 0.05543))
+
+    }
+
+    stepSvgDraw(element: any, progressFunc: any) {
+        var from = element.children[0].children[0].getTotalLength();
+        var to = 0;
+
+        var pro = from + (to-from)*progressFunc;
+        element.style.strokeDasharray = from;
+        element.style.strokeDashoffset = pro;
+    }
+    animateSvg() {
+        const bezier = [{x: .17, y: .67},{x: 1, y: 1}];
+        setInterval(
+        () => animate(document.querySelector('#svg1'), bezier ,this.stepSvgDraw,4000),300);
     }
 
     render() : JSX.Element {
         return(
-            <div id="svg1" onClick={move}>
-                eren
+            <div id="svg1" onClick={this.animateSvg}>
+                <Ucak/>
             </div>
         )
     }
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -2,30 +2,39 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 
+/**
+ * @abstract Gives edges between given control points
+ * @author Eren Dere (yatiyr)
+ * @param controlPoints 
+ */
 function giveEdges(controlPoints) {
     var edges = [];
-
     for(var i=0; i<controlPoints.length -1; i++) {
         var edge = {first: controlPoints[i], second: controlPoints[i+1]};
         edges.push(edge);
     }
-
     return edges;
 }
 
-
+/**
+ * @abstract Gives point on edge wrt a number t between 0,1
+ * @author Eren Dere (yatiyr)
+ * @param edge 
+ * @param t 
+ */
 function giveEdgePoint(edge, t) {
-
     return {x: edge.first.x + t * (edge.second.x - edge.first.x),
             y: edge.first.y + t * (edge.second.y - edge.first.y)};
-
 }
 
-export function givePointOnBezier(controlPoints, t) {
-
+/**
+ * @abstract Gives point on a bezier curve wrt a number t between 0,1
+ * @author Eren Dere (yatiyr)
+ * @param controlPoints 
+ * @param t 
+ */
+function givePointOnBezier(controlPoints, t) {
     var edges = giveEdges(controlPoints);
-
-
     if(edges.length == 1) {
         return giveEdgePoint(edges[0], t);
     }
@@ -35,49 +44,80 @@ export function givePointOnBezier(controlPoints, t) {
             var edge = edges[i];
             newPoints.push(giveEdgePoint(edge, t));
         }
-
         return givePointOnBezier(newPoints, t);
     }
 }
 
-export function timeFunction(controlPoints,t) {
-
-    var points = controlPoints;
-
+/**
+ * @abstract Gives progress of a bezier time function
+ * @author Eren Dere (yatiyr)
+ * @param controlPoints 
+ * @param t 
+ */
+function timeFunction(controlPoints,t) {
+    var points = [...controlPoints];
     var pointf = {x: 0, y: 0};
     var pointl = {x: 1, y: 1};
 
     points.unshift(pointf);
     points.push(pointl);
 
-    return givePointOnBezier(points, t);
+    return givePointOnBezier(points, t).y;
 }
 
 
-export function move() {
-    var elem = document.querySelector('#svg1');
+/*
+    Move fonksiyonu -> duration, frame
+*/
 
+/**
+ * @abstract This function animates step function
+ * @author Eren Dere (yatiyr)
+ * @param element 
+ * @param bezier 
+ * @param step 
+ * @param duration 
+ */
+export function animate(element, bezier, step, duration) {
+
+    var id = setInterval(frame, 5);
+    var progressDuration = duration/5;
+    var progress = 0;
+
+    function frame() {
+        var t = progress/progressDuration;
+        var progressFunc = timeFunction(bezier,t);
+        console.log(progressFunc);
+        if(progress == progressDuration) {
+            step(element,progressFunc);
+            clearInterval(id);
+        }
+        else {
+            progress++;
+            step(element,progressFunc);
+        }
+
+    }
+}
+
+function move() {
+    var elem = document.querySelector('#svg1');
     var progress = 0;
     var id = setInterval(frame, 5);
-  
-    var x = 500
-    
     var from = 0;
-    var to = 500;
-  
+    var to = 300;
     function frame() {
       
-      var t = progress/400;
-      
-      var func = timeFunction([{x: .6, y: 1.43},{x: .64, y: -.44}],t).y;
-      var pro = from + (to-from)*func;
-      
-      if (progress == 400) {
+      var t = progress/200;
+      var func = timeFunction([{x: .6, y: 1.43},{x: .64, y: -.44}],t);
+      var pro = from + (to-from)*func;    
+
+      if (progress == 200) {
+        elem.style.left = pro + 'px';
         clearInterval(id);
       } else {
         progress++;
         elem.style.left = pro + 'px';
       }
     }
-
 }
